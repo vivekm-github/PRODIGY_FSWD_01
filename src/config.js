4bb14b6d70e62c35e5708b491bced0debd0df01d
vivekm-github <vivekm9606555@gmail.com>
2024-07-06 23:53:37 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const connect = mongoose.connect("mongodb://localhost:27017/Login-tut");
+const connect = mongoose.connect("put your mongodb connection link");
 
 // Check database connected or not
 connect.then(() => {
